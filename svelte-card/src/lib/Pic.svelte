574847c1bf351c218b3458cba18344391e59b593
christiankock <christian.kock@igilitysolutions.com>
2022-10-14 00:36:14 -0700

    version 1

---
+++ svelte-card/src/lib/Pic.svelte
@@ -0,0 +1,3 @@
+<div class="relative z-20 w-36 h-36 mt-8 rounded-full border-4 border-neutral-900 overflow-hidden">
+    <img src="./src/assets/Dork.jpeg" alt="Christian Kock">
+</div>
---
+++ svelte-card/src/lib/Section.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let title;
+    export let text;
+    export let bgColor
+
+
+</script>
+
+<div class="bg-{bgColor} p-6 text-neutral-50 flex flex-col gap-1">
+    <h2 class="text-xl leading-7 font-extrabold">{title}</h2>
+    <p class="text-base leading-6 font-medium">{text}</p>
+</div>
